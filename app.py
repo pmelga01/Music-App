eacf771b7dbecf2c46107825877f4ae87412fe16
pmelga01 <pamela.melgar@tufts.edu>
2024-08-18 17:37:29 -0400

    Testing env vars and rendering template index.html page with api response

--- app.py
+++ app.py
@@ -1,14 +1,28 @@
 import flask
 import os
+import requests
+import json
 
 app = flask.Flask(__name__)
+API_KEY = os.getenv("GENIUS_KEY")
+url = 'https://api.genius.com/search?q=Kendrick%20Lamar'
+
+my_headers = {
+    "Authorization": "Bearer " + str(API_KEY)
+}
+
+response = requests.get(url, headers=my_headers)
+
+print(json.dumps(response.json(), indent=4))
 
 @app.route('/')
 def index():
+    content = json.dumps(response.json(), indent=2)
+    test_content = content["response"]
     return flask.render_template(
-        "index.html"
+        "index.html",
+        test=test_content
     )
-
 app.run(
     port=int(os.getenv('PORT', 3000)),
     host=os.getenv('IP', '0.0.0.0'),
